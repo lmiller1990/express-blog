38f3fda042409f180aa18a172ef6ea47f54c121d
lachlan <lachlan.miller.1990@outlook.com>
2017-01-28 16:01:03 +0900

    Initial

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const index   = require(__dirname + "/routes/index.js")
+
+const app = express()
+
+app.use("/", index)
+
+app.use((req, res) => {
+  res.status(404).send("404 error. Page not found") 
+})
+
+app.listen(3000, () => {
+  console.log("App started on port 3000.")
+})
+
+module.exports = app
